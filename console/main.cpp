93fb82d7ee3bdc6abaaa23efcb100283ca4ba2d8
viacheslavkarpenkoju@gmail.com <Universal201912>
2023-12-18 00:04:53 +0200

    Created new Hello world file.

---
+++ console/main.cpp
@@ -0,0 +1,22 @@
+// Include standard c librart
+#include <iostream>
+
+int main() {
+	// First important comment.
+	// Use standard output stream.
+	std::cout << "Hello in the world!!!\n" << std::endl;
+	std::cout << "World is hell!!!" << std::endl;
+	std::cout << "World is pain!!" << std::endl;
+	std::cout << "World is beautiful, and interesting thing!!!\n" << std::endl;
+
+	// Set Urkrainian language for next output.
+	setlocale(LC_CTYPE, "ukr");
+
+	std::cout << "Óêğà¿íñüêà âëàäà - ïîïóë³ñòè!!!" << std::endl;
+	std::cout << "Óêğà¿íö³ öå ïîòóæíà ñèëà!!!" << std::endl;
+	std::cout << "ÑËÀÂÀ ÓÊĞÀ¯Í² - ÃÅĞÎßÌ ÑËÀÂÀ!!!" << std::endl;
+
+	// Hold console open
+	system("pause");
+	
+}
