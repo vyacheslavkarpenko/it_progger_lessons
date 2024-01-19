18f9b4d4f57d8dddf4b3cdf6752e1f32bc54bb3e
viacheslavkarpenkoju@gmail.com <Universal201912>
2024-01-20 00:11:54 +0200

    Exersice_5 Unfinished

---
+++ #5 random numbers and calculator/calculator.cpp
@@ -0,0 +1,68 @@
+﻿// #5 random numbers and calculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+float number1;
+float number2;
+char mathOperator;
+float result;
+string message;
+
+void resultPrinter(char, string);
+void ifElseCalculator(float, float, char);
+void switchCaseCalculator(float, float, char);
+
+int main()
+{
+    cout << "Simple calculator" << endl;
+    
+    cout << "Input float number 1 like 1.2" << endl;
+    cin >> number1;    
+    cout << "Input float number 2 like 1.2" << endl;
+    cin >> number2;
+    
+    cout << "Input one of math operators like '+, -, *, /'" << endl;
+    cin >> mathOperator;
+
+    ifElseCalculator(number1, number2, mathOperator);
+    switchCaseCalculator(number1, number2, mathOperator);
+    
+    system("pause");
+    
+    return 0;
+}
+
+void resultPrinter(float result, string message) {
+    cout << "Printed by " << message << " calculator!" << endl;
+    cout << "The result = " << result << endl;
+}
+
+void ifElseCalculator(float number1, float number2, char mathOperator) {
+    message = "ifElseCalculator";
+
+    if (mathOperator == '+')
+        result = number1 + number2;
+    else if (mathOperator == '-')
+        result = number1 - number2;
+    else if (mathOperator == '*')
+        result = number1 * number2;
+    else if (mathOperator == '/')
+        result = number1 / number2;
+
+    resultPrinter(result, message);
+}
+
+void switchCaseCalculator(float number1, float number2, char mathOperator) {
+    message = "switchCaseCalculator";
+
+    switch (mathOperator) {
+    case '+': result = number1 + number2;
+    case '-': result = number1 - number2;
+    case '*': result = number1 * number2;
+    case '/ ': result = number1 / number2;
+    }
+    
+    resultPrinter(result, message);
+}
