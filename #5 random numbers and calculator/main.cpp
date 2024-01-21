e900cb9f4c9b0a62a6114aa1dd7ddc11f2561a86
viacheslavkarpenkoju@gmail.com <Universal201912>
2024-01-21 14:56:02 +0200

    Fixed logic.

--- #5 random numbers and calculator/main.cpp
+++ #5 random numbers and calculator/main.cpp
@@ -4,58 +4,89 @@
 
 using namespace std;
 
-int menuSelector;
+int menuSelector = 0;
 bool exitProgram = false;
-int exitProgramFlag = 0;
+char exitProgramFlag = '0';
 
+int mainMenu();
+int announcement();
+int runScriptOrExit();
 int executor();
-bool setExitProgramFlag(int);
+bool setExitProgramFlag(char);
 
 int main()
+{
+	
+	do 
+	{
+		runScriptOrExit();
+		if (exitProgram)
+			return 0;
+
+	    announcement();
+		mainMenu();
+		
+		cin >> menuSelector;
+		
+		executor();
+		
+		menuSelector = 0;
+
+	} while (exitProgram == false);
+}
+
+int announcement()
 {
 	cout << "This is an interesting solution of lesson #5." << endl;
 	cout << "Here are some mathimatics opearions." << endl;
 	cout << endl;
-	if (exitProgram == false)
-	{
+
+	return 0;
+}
+
+int mainMenu() {
 		cout << "Calculator demonstration input '1'." << endl;
 		cout << "Other math operations input '2'." << endl;
 		cout << "Math function 'rand()' input '3'." << endl;
 		cout << endl;
 
-		cin >> menuSelector;
-	}
+		return 0;
+}
 
-	executor();
+int runScriptOrExit()
+{
+	cout << "menuSelector is " << menuSelector << endl;
+	cout << endl;
+	cout << "Press '1' to exit, press '0' to select menu selector." << endl;
+	cout << endl;
 
-
-
-	cout << "Press '1' to exit, press '2' to select another menu selector." << endl;
 	cin >> exitProgramFlag;
 
 	setExitProgramFlag(exitProgramFlag);
-	
-	if (exitProgram)
-		return 0;
-	else
-		executor();
 
+	return 0;
 }
+
 
 int executor()
 {
 	if (menuSelector == 1)
+	{
 		calculator();
+	}
 	else if (menuSelector == 2)
+	{
 		mathematicalOperations();
-	
+
+	}
+
 	return 0;
 }
 
-bool setExitProgramFlag(int exitProgramFlag)
+bool setExitProgramFlag(char exitProgramFlag)
 {
 	if (exitProgramFlag == '1')
 		exitProgram = true;
-	
-	return exitProgram;
+
+	return 0;
 }
